6a78f7ac4d8398dd717e755e46f2ea7b116ded90
anvorja <andres.vorja.vorja@gmail.com>
2023-04-21 18:33:49 -0500

    pruebas2

--- sum.test.js
+++ sum.test.js
@@ -8,14 +8,14 @@ test('sums two numbers', () => {
     expect(sum(1, 2)).toBe(3);
 });
 
-const multiply = require('./multiply');
+const multiply = require('./multi');
 test("multiply", () => {
     expect(multiply(2, 3)).toBe(6);
     expect(multiply(0, 5)).toBe(0);
     expect(multiply(-4, 2)).toBe(-8);
   });
 
-  // Prueba de la función de división
+
 const divide = require('./divide');
 test("divide", () => {
     expect(() => divide(2, 0)).toThrow();
